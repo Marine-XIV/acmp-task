4a9a0bdb3bbab4aef346eb9d632990fa821ab337
Marina. <148859858+Marine-XIV@users.noreply.github.com>
2025-01-24 17:12:12 +0300

    ЗАДАЧА №1147   Верхний регистр

---
+++ 1147.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+ 
+using namespace std;
+int main()
+{
+char c ;
+cin >>c;
+ 
+if(c >= 'a' && c <= 'z'){
+        int alnum = c - 'a';
+    c = 'A' + alnum;
+}
+cout << c;
+ 
+  return 0;
+}
