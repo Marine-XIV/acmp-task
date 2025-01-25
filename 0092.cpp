5d3bee8684ff09219107ace1903e7c0b0509a779
Marina. <148859858+Marine-XIV@users.noreply.github.com>
2025-01-25 14:37:39 +0300

    ЗАДАЧА №92	   Журавлики

---
+++ 0092.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int s;
+    cin >> s;
+
+    int p = s / 6; 
+    int k = 2 * (p + p); 
+
+    cout << p << " " << k << " " << p << endl;
+
+    return 0;
+}
